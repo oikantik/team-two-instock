9e3ef3983407dc65eef3ccaa408ee074b41e2240
codingenginear <momosawi12@gmail.com>
2020-11-24 18:11:22 -0500

    created InventoryItem.js and added assets folder to have access to the icons I need

---
+++ client/src/components/InventoryItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const InventoryItem = (props) => {
+  return (
+    <div className="inv-item-details">
+      <div className="inv-item-details__header">
+        <div className="inventory-item-details__"></div>
+
+      </div>
+      <div></div> 
+      
+    </div>
+  );
+};
+
+export default InventoryItem;
