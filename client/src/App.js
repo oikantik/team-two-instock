f287d4c774fe92b6231591e5c66bb170aa3e239b
codingenginear <momosawi12@gmail.com>
2020-11-23 21:04:10 -0500

    npm installed react-router-dom, created pages folder and home component, and setup initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
+import Home from './pages/Home/Home';
+import { BrowserRouter, Switch, Route  } from 'react-router-dom';
 import "./App.scss";
 
 function App() {
   return (
-    <div className="App">Hello World, look at us! We are the bestest!</div>
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/warehouse' component={}></Route>
+          <Route path='/editWarehouse/:warehouseId' component={}></Route>
+          <Route path='/addWarehouse' component={}></Route>
+          <Route path='/inventory' component={}></Route>
+          <Route path='/inventoryItem' component={}></Route>
+          <Route path='/editInventory/:inventoryId' component={}></Route>
+          <Route path='/addInventory' component={}></Route>
+          <Route path='/warehouse/:warehouseId' component={}></Route>
+          <Route path='/inventory/:inventoryId' component={}></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
