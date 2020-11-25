f2dc3d523fd597ceea935976686b8f941463a85b
codingenginear <momosawi12@gmail.com>
2020-11-25 01:41:16 -0500

    completed tablet breakpoint

--- client/src/components/InventoryItem/InventoryItem.js
+++ client/src/components/InventoryItem/InventoryItem.js
@@ -10,12 +10,12 @@ const InventoryItem = (props) => {
           <img src={arrowBackIcon} alt="Arrow Icon" className="inv-item-details__arrow-icon" ></img>
           <h1 className="inv-item-details__title">Television</h1>
         </div>
-        <div className="inv-item-details__edit-div">
+        <button className="inv-item-details__edit-button">
           <svg className="inv-item-details__edit-icon"width="24" height="24" viewBox="0 0 24 24" fill="FFFFFF" xmlns="http://www.w3.org/2000/svg">
             <path className="inv-item-details__edit-icon-path" d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z" fill="#FFFFFF"/>   
           </svg>
           <p className="inv-item-details__edit">Edit</p>
-        </div>     
+        </button>     
       </div>
       <div className="inv-item-details__border"></div>
       <div className="inv-item-details__main">
