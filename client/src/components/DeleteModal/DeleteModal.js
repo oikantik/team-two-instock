1af3a42ad4e232b35d49d53f32a394d543ecf8f8
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-11-24 23:49:42 -0500

    added css for tablet, overlay works now

--- client/src/components/DeleteModal/DeleteModal.js
+++ client/src/components/DeleteModal/DeleteModal.js
@@ -4,28 +4,30 @@ import closeIcon from "../../assets/icons/close-24px.svg";
 
 function DeleteModal() {
   return (
-    <div className="delete-modal">
-      <button className="delete-modal__close">
-        <img
-          src={closeIcon}
-          alt="closeIcon"
-          className="delete-modal__close-image"
-        />
-      </button>
-      <h1 className="delete-modal__headline">
-        DELETE {"warehousen_name"} warehouse?
-      </h1>
-      <p className="delete-modal__paragraph">
-        Please confirm that you'd like to delete the King West from the list of
-        warehouses. You won't be able to undo this action.
-      </p>
-      <div className="delete-modal__buttons">
-        <button className="delete-modal__button delete-modal__button--cancel">
-          Cancel
+    <div className="delete-modal-overlay">
+      <div className="delete-modal">
+        <button className="delete-modal__close">
+          <img
+            src={closeIcon}
+            alt="closeIcon"
+            className="delete-modal__close-image"
+          />
         </button>
-        <button className="delete-modal__button delete-modal__button--delete">
-          Delete
-        </button>
+        <h1 className="delete-modal__headline">
+          DELETE {"warehousen_name"} warehouse?
+        </h1>
+        <p className="delete-modal__paragraph">
+          Please confirm that you'd like to delete the King West from the list
+          of warehouses. You won't be able to undo this action.
+        </p>
+        <div className="delete-modal__buttons">
+          <button className="delete-modal__button delete-modal__button--cancel">
+            Cancel
+          </button>
+          <button className="delete-modal__button delete-modal__button--delete">
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
