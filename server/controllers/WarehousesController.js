2e394233c97915b84b9ada9240ebf64a1274eb1c
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-11-25 14:05:32 -0500

    added routes for warehouse with associated inventories

--- server/controllers/WarehousesController.js
+++ server/controllers/WarehousesController.js
@@ -1,9 +1,19 @@
-const warehousesModel = require('../models/WarehousesModel');
+const warehousesModel = require("../models/WarehousesModel");
 
 const getAllWarehouses = (req, res) => {
-    res.status(200).json(warehousesModel.readFromWarehousesFile());
-}
+  res.status(200).json(warehousesModel.readFromWarehousesFile());
+};
+
+const getSingleWarehouse = (req, res) => {
+  const id = req.params.warehouseId;
+  const warehouse = warehousesModel.findOne(id);
+  if (!warehouse) {
+    return res.status(404).json({ error: "warehouse not found" });
+  }
+  res.status(200).json(warehouse);
+};
 
 module.exports = {
-    getAllWarehouses
-}
+  getSingleWarehouse,
+  getAllWarehouses,
+};
