c9f3bafa4f96e51a123107ed764c3fac117c4ae3
codingenginear <momosawi12@gmail.com>
2020-11-25 16:17:05 -0500

    added a link for the back arrow leading to whole inventory list, and another one for the edit inventory item

--- client/src/components/InventoryItem/InventoryItem.js
+++ client/src/components/InventoryItem/InventoryItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import arrowBackIcon from '../../assets/Icons/arrow_back-24px.svg';
+import { Link } from 'react-router-dom';
 import './InventoryItem.scss';
 
 const InventoryItem = (props) => {
@@ -7,15 +8,20 @@ const InventoryItem = (props) => {
     <div className="inv-item-details">
       <div className="inv-item-details__header">
         <div className="inv-item-details__title-div">
-          <img src={arrowBackIcon} alt="Arrow Icon" className="inv-item-details__arrow-icon" ></img>
+          <Link to={'/inventory'}>
+            <img src={arrowBackIcon} alt="Arrow Icon" className="inv-item-details__arrow-icon" />
+          </Link>
           <h1 className="inv-item-details__title">Television</h1>
         </div>
-        <button className="inv-item-details__edit-button">
-          <svg className="inv-item-details__edit-icon"width="24" height="24" viewBox="0 0 24 24" fill="FFFFFF" xmlns="http://www.w3.org/2000/svg">
-            <path className="inv-item-details__edit-icon-path" d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z" fill="#FFFFFF"/>   
-          </svg>
-          <p className="inv-item-details__edit">Edit</p>
-        </button>     
+        {/* We can add the inventory ID through the props once we patch everything together */}
+        <Link to={`/editInventory/:inventoryId`}>
+          <button className="inv-item-details__edit-button">
+            <svg className="inv-item-details__edit-icon"width="24" height="24" viewBox="0 0 24 24" fill="FFFFFF" xmlns="http://www.w3.org/2000/svg">
+              <path className="inv-item-details__edit-icon-path" d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z" fill="#FFFFFF"/>   
+            </svg>
+            <p className="inv-item-details__edit">Edit</p>
+          </button>
+        </Link>    
       </div>
       <div className="inv-item-details__border"></div>
       <div className="inv-item-details__main">
