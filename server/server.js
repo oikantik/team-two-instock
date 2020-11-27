18018a967ca31b87445d7153f5868ac43ec0fd17
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-11-27 16:52:35 -0500

    changed server.js to accept crud, delete modal deletes warehouse now

--- server/server.js
+++ server/server.js
@@ -15,8 +15,9 @@ const SERVER_PORT = 8080;
 
 //CORS-------------------------------
 
-app.use(function (_req, res, next){
+app.use(function (_req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -31,22 +32,22 @@ app.use('/inventory', inventoryRoutes);
 
 app.use(express.static('files'));
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     console.log('request');
     next();
-}); 
+});
 
 
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     //VERIFYING ACTUAL JSON REQUEST FROM THE CLIENT
-    if (req.method === 'POST' && req.headers ['content-type'] !== 'application/json') {
+    if (req.method === 'POST' && req.headers['content-type'] !== 'application/json') {
         //RETURN ERROR
         return res.status(400).send('Server requires application.json');
     } else {
         next();
     }
-}); 
+});
 
 
 
@@ -54,5 +55,5 @@ app.use((req,res,next) => {
 //START THE SERVER LISTENING-------------------------------------------
 
 app.listen(SERVER_PORT, () => {
-    console.log('HELLO WORLD'); 
+    console.log('HELLO WORLD');
 })
