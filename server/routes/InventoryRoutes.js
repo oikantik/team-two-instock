82f3925142cff7b16779e234bccd0f462e67e62e
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-11-27 17:49:55 -0500

    added endpoint for deleting an inventory

--- server/routes/InventoryRoutes.js
+++ server/routes/InventoryRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const inventoryController = require("../controllers/InventoryController");
 
+router.delete("/:inventoryId", inventoryController.deleteSingleInventory);
 
 // Export the Router------------------------------
 module.exports = router;
