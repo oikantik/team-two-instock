2ab037d8a07d39ce9b635f6b4a9f47d787085913
Ali Akbar <63883870+Cheval5@users.noreply.github.com>
2020-11-28 13:04:19 -0500

    Back-End: API to PUT/PATCH/EDIT an Inventory Item feature/TOR20Q1MD-25

--- server/controllers/InventoryController.js
+++ server/controllers/InventoryController.js
@@ -9,6 +9,19 @@ const deleteSingleInventory = (req, res) => {
   res.status(404).send("Inventory with the given id was not found");
 }
 
+const updateInventory = (req, res) => {
+  const id = req.params.inventoryId;
+  if (!inventoryModel.doesInventoryExist(id)) {
+    return res.status(404).send("Inventory with the given ID was not found.");
+  }
+  if (!inventoryModel.reqBodyIsValid(req.body)) {
+    return res.status(400).send("One or more inputs is empty or invalid.");
+  }
+  const updatedInventory = inventoryModel.updateInventory(id, req.body);
+  return res.status(200).json(updatedInventory);
+}
+
 module.exports = {
-  deleteSingleInventory
+  deleteSingleInventory,
+  updateInventory
 };
--- server/routes/InventoryRoutes.js
+++ server/routes/InventoryRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const inventoryController = require("../controllers/InventoryController");
 
+router.put("/:inventoryId", inventoryController.updateInventory);
 router.delete("/:inventoryId", inventoryController.deleteSingleInventory);
 
 // Export the Router------------------------------
